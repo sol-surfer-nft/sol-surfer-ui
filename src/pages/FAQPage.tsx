ce89c09dbd76a239c56f794cbcacbf93bb46c340
Nico Stranquist <nstranquist@gmail.com>
2021-06-05 21:04:09 -0500

    write v1 faq questions and answers, populate faq page

--- src/pages/FAQPage.tsx
+++ src/pages/FAQPage.tsx
@@ -1,14 +1,42 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Typography } from 'antd'
 import { PageHeader } from '../components/PageHeader/PageHeader'
+import { faqItems } from '../data/faq.data'
 
 const FAQPage = () => {
 
 
   return (
-    <div>
-      <PageHeader title="FAQ Page!" />
-    </div>
+    <StyledFAQPage>
+      <PageHeader title="Questions and Answers" />
+
+      <div className="faq-container">
+        {faqItems.map(faqItem => (
+          <div className="faq-item" key={faqItem.questionId}>
+            <Typography.Title level={2} className="question-title">{faqItem.questionTitle}</Typography.Title>
+            <Typography.Paragraph className="question-answer">{faqItem.questionAnswer}</Typography.Paragraph>
+          </div>
+        ))}
+      </div>
+    </StyledFAQPage>
   )
 }
+
+const StyledFAQPage = styled.div`
+  .faq-container {
+    width: 50%;
+    margin: auto;
+    margin-bottom: 4rem;
+  }
+  .faq-item {
+    margin-bottom: 2rem;
+    padding: 2rem;
+
+    .question-answer {
+      font-size: 22px;
+    }
+  }
+`
 
 export default FAQPage
