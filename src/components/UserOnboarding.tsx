00f027390869770cb723bfa660e98cbd1fc907b7
Nico Stranquist <nstranquist@gmail.com>
2021-06-13 11:26:19 -0500

    fix warning

--- src/components/UserOnboarding.tsx
+++ src/components/UserOnboarding.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Link, useHistory } from 'react-router-dom'
+import { Link, /*useHistory*/ } from 'react-router-dom'
 import { useRecoilState } from 'recoil'
 import styled from 'styled-components'
 import { Typography, Button } from 'antd'
