465786f6941a6f6566ddca161caa891ad93efaec
Nico Stranquist <nstranquist@gmail.com>
2021-06-29 22:48:08 -0500

    almost get transactions working

--- src/pages/FaucetPage.tsx
+++ src/pages/FaucetPage.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
-import { Button, Typography } from 'antd'
-import { LAMPORTS_PER_SOL } from '@solana/web3.js';
+import { Button, Typography, Input, InputNumber } from 'antd'
 import { PageHeader } from '../components/PageHeader/PageHeader'
 import { useWallet } from '../contexts/wallet'
 import { useConnection } from '../contexts/connection'
@@ -9,13 +8,25 @@ import { useNativeAccount } from '../contexts/accounts'
 import { formatNumber } from '../utils/utils';
 import { notify } from "../utils/notifications";
 
+import { SystemProgram, Account, Transaction, PublicKey, TransactionInstruction, LAMPORTS_PER_SOL } from '@solana/web3.js';
+
 const SOL_PER_AIRDROP = 2
+
+const TEST_RECIPIENT_ADDRESS = "4FvypZ9Q9e2Hg6Y5nNMfq5dadQbLBuA24msh4PAcpiMA"
+const TEST_ADDRESS_2 = "HwsLSSa6sHpAcxxHUskK9AZLKmin4HRbswcokGSJM6dq"
+const TEST_ADDRESS_SOLFLARE = "4sam79hQK2QNaWitt9cedK5jvBCc52YNMyaCedTV9Dh5"
 
 const FaucetPage = () => {
   const { wallet, connected } = useWallet();
   const { account } = useNativeAccount();
   const connection = useConnection()
-  const [loading, setLoading] = useState(false)
+
+  const [loadingFaucet, setLoadingFaucet] = useState(false)
+  const [loadingTransaction, setLoadingTransaction] = useState(false)
+  const [formData, setFormData] = useState({
+    recipientAddress: TEST_RECIPIENT_ADDRESS,
+    amount: 1
+  })
 
   useEffect(() => {
     if(!connected) {
@@ -30,7 +41,7 @@ const FaucetPage = () => {
   const getFaucet = async () => {
     console.log('getting faucet')
     if(!wallet?.publicKey) return;
-    setLoading(true)
+    setLoadingFaucet(true)
     try {
       let response = await connection.requestAirdrop(wallet.publicKey, SOL_PER_AIRDROP * LAMPORTS_PER_SOL)
       if(response) {
@@ -49,11 +60,103 @@ const FaucetPage = () => {
         type: "error"
       })
     }
-    setLoading(false)
+    setLoadingFaucet(false)
   }
 
   const getSolAmount = () => {
     return formatNumber.format((account?.lamports || 0) / LAMPORTS_PER_SOL)
+  }
+
+  const handleChange = (e) => {
+    setFormData(data => ({ ...data, [e.target.name]: [e.target.value] }))
+  }
+  const handleNumberChange = (value) => {
+    setFormData(data => ({ ...data, amount: Number(value) }))
+  }
+  const printFormData = () => {
+    console.log('-----FORM DATA-----')
+    Object.keys(formData).forEach(key => {
+      console.log(`${key}: ${formData[key]}`)
+    })
+  }
+
+  const sendTransaction = async () => {
+    if(!formData.amount || !formData.recipientAddress) {
+      notify({ type: "error", message: "Missing recipient address or amount to send. Please fix and try again" });
+      return;
+    }
+    else {
+      printFormData()
+    }
+
+    notify({ type: "info", message: "Sending transaction" })
+    setLoadingTransaction(true)
+
+    try {
+      // Get Wallets and Accounts info needed for transaction
+      const destinationPubkey = new PublicKey(formData.recipientAddress)
+      const walletAccountInfo = await connection.getAccountInfo(wallet!.publicKey!)
+      console.log('destination pubkey:', destinationPubkey)
+      console.log("wallet data size", walletAccountInfo?.data.length);
+      
+      const receiverAccountInfo = await connection.getAccountInfo(destinationPubkey);
+      console.log('receiver account info:', receiverAccountInfo)
+      console.log("receiver data size", receiverAccountInfo?.data.length);
+
+      const lamportsToSend = formData.amount * LAMPORTS_PER_SOL;
+
+      console.log('wallet pub key:', wallet!.publicKey!)
+      // Prepare instruction
+      const instruction = SystemProgram.transfer({
+        fromPubkey: wallet!.publicKey!,
+        toPubkey: destinationPubkey,
+        lamports: lamportsToSend 
+      });
+
+      console.log('instruction:', instruction)
+
+      // Prepare transaction
+      let transaction = new Transaction();
+      transaction.add(instruction);
+      transaction.feePayer = wallet!.publicKey!;
+      let hash = await connection.getRecentBlockhash();
+      console.log('blockhash:', hash)
+      transaction.recentBlockhash = hash.blockhash;
+
+      console.log('transaction:', transaction)
+
+      // Prepare Signature
+      const signedTransaction = await wallet!.signTransaction(transaction);
+      console.log('signed transaction:', signedTransaction)
+      const signature = await connection.sendRawTransaction(signedTransaction.serialize())
+      console.log('sent raw transaction')
+
+      // Get Result, Output Results
+      const result = await connection.confirmTransaction(signature, "singleGossip")
+      console.log('transaction result (LAMPORTS were sent):', result)
+    }
+    catch (error) {
+      console.log("error sending transaction:", error)
+      notify({ type: "error", message: "Error sending transaction: " + error.toString() })
+    }
+
+    setLoadingTransaction(false)
+
+    // const minimumAmount = await connection.getMinimumBalanceForRentExemption(0)
+
+    // const transactionInstruction = new TransactionInstruction({
+    //   keys: [{ pubkey: destPubkey, isSigner: false, isWritable: true }],
+    //   programId
+    // })
+
+    // const connection = new Connection(url, 'singleGossip')
+    // const sender = new Account()
+    // const recipient = new Account()
+
+    
+    // 1. const signature = await connection.sendTransaction(transaction, signers, sendOptions)
+    // 2. const status = await connection.confirmTransaction(signature, options?); let { value, err } = status;
+    // 3. return signature;
   }
 
   return (
@@ -66,9 +169,23 @@ const FaucetPage = () => {
       </div>
 
       <div className="faucet-section">
-        <Button onClick={getFaucet} disabled={loading}>{loading ? "..." : "Get Faucet"}</Button>
+        <Button onClick={getFaucet} disabled={loadingFaucet}>{loadingFaucet ? "..." : "Get Faucet"}</Button>
         {' '}
         <span style={{marginLeft: 8}}>{SOL_PER_AIRDROP} SOL per faucet</span>
+      </div>
+
+      {/* Send Transaction */}
+      <div className="faucet-section">
+        <Typography.Title level={4}>Send a Transaction</Typography.Title>
+
+        <div className="faucet-subsection">
+          <Input value={formData.recipientAddress} onChange={handleChange} name="recipientAddress" placeholder="Recipient Address" />
+          <br />
+          <InputNumber min={0.00000001} max={10000} value={formData.amount} onChange={handleNumberChange} placeholder="Amount" name="amount" />
+          <br />
+          <br />
+          <Button onClick={sendTransaction} disabled={loadingTransaction}>{loadingFaucet ? "..." : "Send Transaction"}</Button>
+        </div>
       </div>
     </StyledFaucetPage>
   )
@@ -78,6 +195,9 @@ const StyledFaucetPage = styled.div`
   .faucet-section {
     padding-top: 2rem;
     padding-bottom: 2rem;
+  }
+  .faucet-subsection {
+
   }
 `
 
